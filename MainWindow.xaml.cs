ee106925074e397d4e2d0958e73c8482eceb6e04
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2020-11-06 20:21:45 +0300

    Вернул всё в рабочее состояние

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.ComponentModel;
 using System.IO;
 using Microsoft.Win32;
 using System.Linq;
+using System.Windows.Controls.Primitives;
+using System.Windows.Markup;
 
 namespace InventoryControl
 {
@@ -16,47 +18,82 @@ namespace InventoryControl
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-
+        public List<ProductData> Products = new List<ProductData>();
         public MainWindow()
         {
             InitializeComponent();
 
-            
-        }
+            for (int z = 0; z < 17; z++)
+            {
+                Products.Add(ProductDatabase.GetProductData(z));
+            }
+            MainDataGrid.ItemsSource = Products;
 
-        private void MakeSearch(string searchString, List<ProductData> products)
+            var firstColumn = GenerateColumn("N", "Номер по прайсу", "Id", "3*");
+            firstColumn.SortDirection = ListSortDirection.Ascending;
+            MainDataGrid.Columns.Add(firstColumn);
+            MainDataGrid.Columns.Add(GenerateColumn("Наименование", "Наименование", "Title", "12*"));
+            MainDataGrid.Columns.Add(GenerateColumn("Кол.", "Количество", "Number", "2*"));
+            MainDataGrid.Columns.Add(GenerateColumn("Вес", "Вес", "Weight", "3*"));
+            MainDataGrid.Columns.Add(GenerateColumn("Ед. изм.", "Единица измерения", "Measurement", "3*"));
+            MainDataGrid.Columns.Add(GenerateColumn("Закуп. цена", "Закупочная цена", "PurchasePrice", "4*"));
+            MainDataGrid.Columns.Add(GenerateColumn("Прод. цена", "Продажная цена", "SalePrice", "4*"));
+
+            MainDataGrid.Columns[0].DisplayIndex = MainDataGrid.Columns.Count - 1;
+        }
+        private DataGridColumn GenerateColumn(String headerTitle, String headerTooltip, String Id, String width)
         {
-            var dataGrid = (DataGrid)MainDataGrid.Content;
-            
+            DataGridTemplateColumn column = new DataGridTemplateColumn();
+            column.Header = headerTitle;
+            column.HeaderStyle = new Style(typeof(DataGridColumnHeader));
+            column.HeaderStyle.BasedOn = (Style)FindResource("MahApps.Styles.DataGridColumnHeader");
+            column.HeaderStyle.Setters.Add(new Setter(ToolTipService.ToolTipProperty, headerTooltip));
+            column.HeaderStyle.Setters.Add(new Setter(HorizontalContentAlignmentProperty, HorizontalAlignment.Center));
+            column.HeaderStyle.Setters.Add(new Setter(TextBlock.TextWrappingProperty, TextWrapping.Wrap));
+
+            column.SetValue(TextBlock.TextWrappingProperty, TextWrapping.Wrap);
+
+            var widthConverter = new DataGridLengthConverter();
+            column.Width = (DataGridLength)widthConverter.ConvertFromString(width);
+
+            var CellDataTemplate = @"
+            <DataTemplate
+                xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"" xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"" xmlns:util=""clr-namespace:InventoryControl.Util;assembly=InventoryControl"">
+                <util:HighlightTextBlock Text = ""{Binding " + Id + @"}"" HighlightPhrase=""{Binding ElementName=Searchbox, Path=Text}"" HighlightBrush=""Orange""/>
+            </DataTemplate>";
+            column.CellTemplate = (DataTemplate)XamlReader.Parse(CellDataTemplate);
+            column.SortMemberPath = Id;
+
+            return column;
+        }
+        private void MakeSearch(string searchString)
+        {
             int sortIndex = 0;
             ListSortDirection? direction = null;
-            for (int i = 0; i < dataGrid.Columns.Count; i++)
+            for (int i = 0; i < MainDataGrid.Columns.Count; i++)
             {
-                if (dataGrid.Columns[i].SortDirection != null)
+                if (MainDataGrid.Columns[i].SortDirection != null)
                 {
                     sortIndex = i;
-                    direction = dataGrid.Columns[i].SortDirection;
+                    direction = MainDataGrid.Columns[i].SortDirection;
                     break;
                 }
             }
 
             var filtered =  new List<ProductData>();
-            foreach (ProductData product in products)
+            foreach (ProductData product in Products)
             {
                 String search = Searchbox.Text.ToLower().Replace('ё', 'е').Trim();
                 String title = product.Title.ToLower().Replace('ё', 'е').Trim();
 
                 if (title.Contains(search)) { filtered.Add(product); }
             }
-            dataGrid.ItemsSource = filtered;
-            dataGrid.Columns[sortIndex].SortDirection = direction;
+            MainDataGrid.ItemsSource = filtered;
+            MainDataGrid.Columns[sortIndex].SortDirection = direction;
         }
 
 
-        private void SearchBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-            
-        }
+        private void SearchBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e) { MakeSearch(Searchbox.Text); }
         private void LoadBackupButtonClick(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
--- UserControls/ProductDataGrid.xaml.cs
+++ UserControls/ProductDataGrid.xaml.cs
@@ -23,55 +23,14 @@ namespace InventoryControl.UserControls
     /// </summary>
     public partial class ProductDataGrid : UserControl
     {
-        public List<ProductData> Products = new List<ProductData>();
 
         public ProductDataGrid()
         {
             InitializeComponent();
 
-            for (int z = 0; z < 17; z++)
-            {
-                Products.Add(ProductDatabase.GetProductData(z));
-            }
-            MainDataGrid.ItemsSource = Products;
-
-            var firstColumn = GenerateColumn("N", "Номер по прайсу", "Id", "3*");
-            firstColumn.SortDirection = ListSortDirection.Ascending;
-            MainDataGrid.Columns.Add(firstColumn);
-            MainDataGrid.Columns.Add(GenerateColumn("Наименование", "Наименование", "Title", "12*"));
-            MainDataGrid.Columns.Add(GenerateColumn("Кол.", "Количество", "Number", "2*"));
-            MainDataGrid.Columns.Add(GenerateColumn("Вес", "Вес", "Weight", "3*"));
-            MainDataGrid.Columns.Add(GenerateColumn("Ед. изм.", "Единица измерения", "Measurement", "3*"));
-            MainDataGrid.Columns.Add(GenerateColumn("Закуп. цена", "Закупочная цена", "PurchasePrice", "4*"));
-            MainDataGrid.Columns.Add(GenerateColumn("Прод. цена", "Продажная цена", "SalePrice", "4*"));
-
-            MainDataGrid.Columns[0].DisplayIndex = MainDataGrid.Columns.Count - 1;
+            
         }
-        private DataGridColumn GenerateColumn(String headerTitle, String headerTooltip, String Id, String width)
-        {
-            DataGridTemplateColumn column = new DataGridTemplateColumn();
-            column.Header = headerTitle;
-            column.HeaderStyle = new Style(typeof(DataGridColumnHeader));
-            column.HeaderStyle.BasedOn = (Style)FindResource("MahApps.Styles.DataGridColumnHeader");
-            column.HeaderStyle.Setters.Add(new Setter(ToolTipService.ToolTipProperty, headerTooltip));
-            column.HeaderStyle.Setters.Add(new Setter(HorizontalContentAlignmentProperty, HorizontalAlignment.Center));
-            column.HeaderStyle.Setters.Add(new Setter(TextBlock.TextWrappingProperty, TextWrapping.Wrap));
-
-            column.SetValue(TextBlock.TextWrappingProperty, TextWrapping.Wrap);
-
-            var widthConverter = new DataGridLengthConverter();
-            column.Width = (DataGridLength)widthConverter.ConvertFromString(width);
-
-            var CellDataTemplate = @"
-            <DataTemplate
-                xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"" xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"" xmlns:util=""clr-namespace:InventoryControl.Util;assembly=InventoryControl"">
-                <util:HighlightTextBlock Text = ""{Binding " + Id + @"}"" HighlightPhrase=""{Binding ElementName=Searchbox, Path=Text}"" HighlightBrush=""Orange""/>
-            </DataTemplate>";
-            column.CellTemplate = (DataTemplate)XamlReader.Parse(CellDataTemplate);
-            column.SortMemberPath = Id;
-
-            return column;
-        }
+        
 
         private void SendButtonClicked(object sender, RoutedEventArgs e)
         {
