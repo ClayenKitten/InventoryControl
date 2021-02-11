bd271a11504b4a16af0810183fdbad5dea570d34
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2021-02-11 19:26:48 +0300

    LogicalTreeHelperExt implemented

---
+++ Util/LogicalTreeHelperExt.cs
@@ -0,0 +1,26 @@
+﻿using SharedControls.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace InventoryControl.Util
+{
+    public static class LogicalTreeHelperExt
+    {
+		public static IEnumerable<T> FindChildrenElementsOfType<T>(DependencyObject current)
+		{
+			List <T> res = new List<T>();
+			res.AddRange(LogicalTreeHelper.GetChildren(current).OfType<T>());
+			foreach (var child in LogicalTreeHelper.GetChildren(current).OfType<DependencyObject>())
+			{
+				res.AddRange(FindChildrenElementsOfType<T>(child));
+			}
+			return res;
+		}
+	}
+}
