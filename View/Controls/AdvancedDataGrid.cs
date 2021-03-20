aab233b95f819957025200146582a924c019d2f8
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2021-03-20 12:20:53 +0300

    Implement filtering in AdvancedDataGrid

--- View/Controls/AdvancedDataGrid.cs
+++ View/Controls/AdvancedDataGrid.cs
@@ -1,3 +1,4 @@
+using InventoryControl.Model;
 using System;
 using System.ComponentModel;
 using System.Windows.Controls;
@@ -8,22 +9,25 @@ namespace InventoryControl.View.Controls
     public class AdvancedDataGrid : DataGrid
     {
         private string searchString;
-        //TODO
         public string SearchString
         {
             get => searchString;
             set
             {
                 searchString = value;
-                Items.Filter = (x) => { return true; };
+                Items.Filter = (x) =>
+                {
+                    bool hasPassed = true;
+                    if (x is INamed)
+                        hasPassed = (x as INamed).Name == SearchString;
+                    else
+                        hasPassed = false;
+                    return hasPassed && this.Filter(x);
+                };
             }
         }
-        
-        public AdvancedDataGrid() : base()
-        {
-            
-            new CollectionViewSource() { Source = this.ItemsSource };
-            
-        }
+        public Predicate<object> Filter { get; set; }      
+
+        public AdvancedDataGrid() : base() { }
     }
 }
