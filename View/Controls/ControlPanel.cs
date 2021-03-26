366bf1073d1e655b941bc5d596597f6e8004b5b2
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2021-03-26 20:18:05 +0300

    Add passing Dispose function to DataContext

--- View/Controls/ControlPanel.cs
+++ View/Controls/ControlPanel.cs
@@ -1,10 +1,21 @@
 using System;
+using System.ComponentModel;
 using System.Windows.Controls;
 
 namespace InventoryControl.View.Controls
 {
     public class ControlPanel : UserControl, IDisposable
     {
-        public virtual void Dispose() { }
+        public virtual void Dispose() 
+        {
+            if(this.DataContext is IDisposable)
+            {
+                ((IDisposable)this.DataContext).Dispose();
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine($"Datacontext of {this.GetType().Name} is not IDisposable!");
+            }
+        }
     }
 }
