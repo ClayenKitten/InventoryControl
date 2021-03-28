1510c88ffeb8e45ec56ad0c3737377aaa8316144
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2021-03-28 15:30:40 +0300

    Refactor ContextMenuBuilder

--- View/Controls/ContextMenuBuilder.cs
+++ View/Controls/ContextMenuBuilder.cs
@@ -9,30 +9,42 @@ namespace InventoryControl.View.Controls
         private ContextMenuBuilder parentBuilder;
         private ItemsControl item;
 
-        public ContextMenuBuilder AddAction(string header, Action onClicked)
-        {
-            var cmb = new ContextMenuBuilder(this, header, isCheckable: false, isEnabled: true);
-            ((MenuItem)cmb.item).Click += (sender, e) => { onClicked?.Invoke(); };
-            return cmb;
-        }
-        public ContextMenuBuilder AddCommand(string header, ICommand onClicked)
-        {
-            var cmb = new ContextMenuBuilder(this, header, isCheckable: false, isEnabled: true);
-            ((MenuItem)cmb.item).Click += (sender, e) => { onClicked?.Execute(null); };
-            return cmb;
-        }
-        public ContextMenuBuilder AddCheckable(string header, Action<bool> onClicked)
-        {
-            var cmb = new ContextMenuBuilder(this, header, isCheckable: true, isEnabled: true);
-            ((MenuItem)cmb.item).Click += (sender, e) => { onClicked?.Invoke(((MenuItem)cmb.item).IsChecked); };
-            return cmb;
-        }
-
-        public ContextMenuBuilder Commit()
+        public ContextMenuBuilder BeginGroup(string header)
+            => new ContextMenuBuilder(this, header);
+        public ContextMenuBuilder EndGroup()
         {
             parentBuilder.item.Items.Add(item);
             return parentBuilder;
         }
+
+        public ContextMenuBuilder AddAction(string header, Action onClicked)
+        {
+            var menuItem = new MenuItem() { Header = header };
+            menuItem.Click += (sender, e) => { onClicked?.Invoke(); };
+            item.Items.Add(menuItem);
+            return this;
+        }
+        public ContextMenuBuilder AddCommand(string header, ICommand onClicked)
+        {
+            var menuItem = new MenuItem() { Header = header };
+            menuItem.Click += (sender, e) => { onClicked?.Execute(null); };
+            item.Items.Add(menuItem);
+            return this;
+        }
+        public ContextMenuBuilder AddCheckable(string header, Action<bool> onClicked)
+        {
+            var menuItem = new MenuItem() { Header = header, IsCheckable = true, StaysOpenOnClick = true };
+            menuItem.Click += (sender, e) => { onClicked?.Invoke(menuItem.IsChecked); };
+            item.Items.Add(menuItem);
+            return this;
+        }
+        public ContextMenuBuilder AddSeparator()
+        {
+            item.Items.Add(new Separator());
+            return this;
+        }
+
+
         public ContextMenu Build()
         {
             var ctxMenuBuilder = this;
@@ -43,19 +55,14 @@ namespace InventoryControl.View.Controls
             return (ContextMenu)ctxMenuBuilder.item;
         }
 
-        private ContextMenuBuilder(ContextMenuBuilder parent, string header, bool isCheckable, bool isEnabled)
+        private ContextMenuBuilder(ContextMenuBuilder parent, string header)
         {
             parentBuilder = parent;
-            item = new MenuItem()
-            {
-                Header = header,
-                IsCheckable = isCheckable,
-                StaysOpenOnClick = isCheckable,
-                IsEnabled = isEnabled,
-            };
+            item = new MenuItem() { Header = header };
         }
         public ContextMenuBuilder()
         {
+            parentBuilder = null;
             item = new ContextMenu();
         }
     }
