616114a29200178bd12eef8416dcd3c348a650b1
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2021-03-30 00:30:28 +0300

    Replace SqlType enum with class

--- ORM/SqlType.cs
+++ ORM/SqlType.cs
@@ -1,11 +1,31 @@
-﻿namespace InventoryControl.ORM
+﻿using System;
+
+namespace InventoryControl.ORM
 {
-    public enum SqlType
+    public class SqlType
     {
-        BOOLEAN,
-        INTEGER,
-        REAL,
-        TEXT,
-        DATETIME,
+        public Type UnderlyingType { get; }
+        public string StringRepresentation { get; }
+
+        public override string ToString()
+            => StringRepresentation;
+
+        private SqlType(Type underlyingType, string stringRepresentation)
+        {
+            UnderlyingType = underlyingType;
+            StringRepresentation = stringRepresentation;
+        }
+
+        public static SqlType BOOLEAN
+            => new SqlType(typeof(bool), "BOOLEAN");
+        public static SqlType INTEGER
+            => new SqlType(typeof(int), "INTEGER");
+        public static SqlType REAL
+            => new SqlType(typeof(double), "REAL");
+        public static SqlType TEXT
+            => new SqlType(typeof(string), "TEXT");
+        public static SqlType DATETIME
+            => new SqlType(typeof(DateTime), "DATETIME");
+
     }
 }
