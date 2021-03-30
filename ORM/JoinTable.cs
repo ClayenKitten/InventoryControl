ac6e75598fddafde5e192924c8aa4016df29967e
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2021-03-31 00:18:20 +0300

    Update JoinTable
    Add Create method;
    Add Update method;
    Update CreationString

--- ORM/JoinTable.cs
+++ ORM/JoinTable.cs
@@ -23,16 +23,33 @@ namespace InventoryControl.ORM
                 var str =
                     $"CREATE TABLE IF NOT EXISTS {Name} " +
                     $"(" +
-                    $"{fname}Id INTEGER REFERENCES {fname} (Id)," +
-                    $"{sname}Id INTEGER REFERENCES {sname} (Id)," +
-                    $"{valueColumn.CreationString}" +
-                    $")";
+                    $"{fname}Id INTEGER REFERENCES {fname} (Id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL," +
+                    $"{sname}Id INTEGER REFERENCES {sname} (Id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL," +
+                    $"{valueColumn.CreationString}, " +
+                    $"UNIQUE ({fname}, {sname}) ON CONFLICT ROLLBACK)";
                 return str;
             }
         }
 
         private Column<IEntity> valueColumn;
 
+        public void Create(int firstId, int secondId, object value)
+        {
+            var fname = firstType.Name;
+            var sname = secondType.Name;
+            var commandText = $"INSERT INTO {Name} ({fname}, {sname}, {valueColumn.Name}) VALUES ({firstId}, {secondId}, $value);";
+            Database.CommitScalarTransaction(commandText, new SQLiteParameter("$value", value));
+        }
+        public void Update(int firstId, int secondId, object value)
+        {
+            var fname = firstType.Name;
+            var sname = secondType.Name;
+            var commandText = 
+            $"UPDATE {Name} " +
+            $"SET {valueColumn.Name}=$value " +
+            $"WHERE {fname}={firstId} AND {sname}={secondId};";
+            Database.CommitScalarTransaction(commandText, new SQLiteParameter("$value", value));
+        }
         public object Read(int firstId, int secondId)
         {
             var fname = firstType.Name;
@@ -46,6 +63,7 @@ namespace InventoryControl.ORM
                 new SQLiteParameter("$secondId", secondId)
             );
         }
+
         public JoinTable(string name, Type first, Type second, Column<IEntity> valueColumn)
         {
             Name = name;
