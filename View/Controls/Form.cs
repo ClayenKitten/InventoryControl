d530540ab63ebb84d338243ac71f936558d440a9
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2021-04-02 16:57:44 +0300

    Create Form and FormConfirmButton

---
+++ View/Controls/Form.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace InventoryControl.View.Controls
+{
+    public class Form : Grid
+    {
+        static Form()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Form), new FrameworkPropertyMetadata(typeof(Form)));
+        }
+
+        public void ClearAllTextBoxes()
+        {
+            foreach (var elem in Children)
+            {
+                if(elem is TextBox)
+                {
+                    (elem as TextBox).SetCurrentValue(TextBox.TextProperty, "");
+                }
+                else if(elem is AdvancedTextbox)
+                {
+                    (elem as AdvancedTextbox).Text = "";
+                }
+            }
+        }
+        public void Confirm()
+        {
+            Confirmed?.Invoke(this, EventArgs.Empty);
+        }
+        public event EventHandler Confirmed;
+    }
+}
---
+++ View/Controls/FormConfirmButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace InventoryControl.View.Controls
+{
+    public class FormConfirmButton : Button
+    {
+        static FormConfirmButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FormConfirmButton), new FrameworkPropertyMetadata(typeof(FormConfirmButton)));
+        }
+        protected override void OnClick()
+        {
+            base.OnClick();
+            DependencyObject control = this;
+            while (control != null && control.GetType() != typeof(Form))
+            {
+                control = VisualTreeHelper.GetParent(this);
+            }
+            if (control.GetType() == typeof(Form))
+            {
+                (control as Form).Confirm();
+            }
+        }
+    }
+}
