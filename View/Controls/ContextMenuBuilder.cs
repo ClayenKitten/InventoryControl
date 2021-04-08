d80aba72830441ae72b32dacdb5b24c698fbf4e7
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2021-04-08 22:14:01 +0300

    Update ContextMenuBuilder.cs

--- View/Controls/ContextMenuBuilder.cs
+++ View/Controls/ContextMenuBuilder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -9,12 +10,44 @@ namespace InventoryControl.View.Controls
         private ContextMenuBuilder parentBuilder;
         private ItemsControl item;
 
+        private List<Tuple<string, MenuItem, Action<bool>>> radioGroups = new List<Tuple<string, MenuItem, Action<bool>>>();
+
         public ContextMenuBuilder BeginGroup(string header)
             => new ContextMenuBuilder(this, header);
         public ContextMenuBuilder EndGroup()
         {
             parentBuilder.item.Items.Add(item);
             return parentBuilder;
+        }
+
+        public ContextMenuBuilder Check(bool isChecked = true)
+        {
+            ((MenuItem)item.Items.GetItemAt(item.Items.Count - 1)).IsChecked = isChecked;
+            return this;
+        }
+
+        public ContextMenuBuilder AsRadioGroup()
+        {
+            foreach (var clicked in radioGroups)
+            {
+                clicked.Item2.Checked += (_, _1) =>
+                {
+                    if (!clicked.Item2.IsChecked)
+                    {
+                        clicked.Item2.IsChecked = true;
+                        return;
+                    }
+                    foreach (var radioGroup in radioGroups)
+                    {
+                        if (radioGroup.Item1 == clicked.Item1)
+                        {
+                            radioGroup.Item2.IsChecked = radioGroup.Item2 == clicked.Item2;
+                            radioGroup.Item3?.Invoke(radioGroup.Item2.IsChecked);
+                        }
+                    }
+                };
+            }
+            return this;
         }
 
         public ContextMenuBuilder AddAction(string header, Action onClicked, bool isEnabled = true)
@@ -41,6 +74,20 @@ namespace InventoryControl.View.Controls
                 IsEnabled = isEnabled
             };
             menuItem.Click += (sender, e) => { onClicked?.Invoke(menuItem.IsChecked); };
+            item.Items.Add(menuItem);
+            return this;
+        }
+
+        public ContextMenuBuilder AddRadio(string header, string radioGroup, Action<bool> OnSet, bool isEnabled = true)
+        {
+            var menuItem = new MenuItem()
+            {
+                Header = header,
+                IsCheckable = true,
+                StaysOpenOnClick = true,
+                IsEnabled = isEnabled,
+            };
+            radioGroups.Add(new Tuple<string, MenuItem, Action<bool>>(radioGroup, menuItem, OnSet));
             item.Items.Add(menuItem);
             return this;
         }
