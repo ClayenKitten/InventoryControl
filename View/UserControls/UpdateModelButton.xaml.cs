528fb522a498a9f87c822ed97676944ce32da0a5
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2021-04-08 20:33:18 +0300

    Create UpdateModelButton usercontrol

---
+++ View/UserControls/UpdateModelButton.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+namespace InventoryControl.View.Controls
+{
+    /// <summary>
+    /// Interaction logic for UpdateModelButton.xaml
+    /// </summary>
+    public partial class UpdateModelButton : Button
+    {
+        public UpdateModelButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
