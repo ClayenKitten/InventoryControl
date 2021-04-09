50de9523477e30c9f4900f64b45a606c7caa6801
EragonRussia <68070406+EragonRussia@users.noreply.github.com>
2021-04-09 23:42:49 +0300

    Create text adorner and use in EditOrganizationPanel

--- View/EditOrganizationPanel.xaml.cs
+++ View/EditOrganizationPanel.xaml.cs
@@ -1,18 +1,28 @@
 ﻿using InventoryControl.Model;
 using InventoryControl.View.Controls;
 using InventoryControl.ViewModel;
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace InventoryControl.View
 {
     /// <summary>
     /// Interaction logic for EditOrganizationPanel.xaml
     /// </summary>
-    public partial class EditOrganizationPanel : ControlPanel
+    public partial class EditOrganizationPanel : ControlPanel, INotifyPropertyChanged
     {
+        private TextAdorner adorner;
         public EditOrganizationPanel()
-        {            
+        {
             InitializeComponent();
+            adorner = new TextAdorner(this);
+        }
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            AdornerLayer.GetAdornerLayer(this).Add(adorner);
+            adorner.Text = "Выберите организацию из списка";
         }
 
         public Counterparty CounterpartyData
@@ -28,6 +38,18 @@ namespace InventoryControl.View
             if(sender is CounterpartyViewer)
             {
                 CounterpartyData = Counterparty.Table.Read((long)message);
+                ContentVisibility = Visibility.Visible;
+                adorner.Text = "";
+            }
+        }
+        private Visibility visibility = Visibility.Collapsed;
+        public Visibility ContentVisibility
+        {
+            get => visibility;
+            set
+            {
+                visibility = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ContentVisibility"));
             }
         }
 
@@ -36,5 +58,6 @@ namespace InventoryControl.View
             Counterparty.Table.Update(CounterpartyData);
             GlobalCommands.ModelUpdated.Execute(null);
         }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
---
+++ View/TextAdorner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace InventoryControl.View
+{
+    public class TextAdorner : Adorner
+    {
+        string text = "";
+        public string Text
+        {
+            get => text;
+            set
+            {
+                text = value;
+                InvalidateVisual();
+            }
+        }
+        public TextAdorner(UIElement elem) : base(elem)
+        {
+            SetValue(IsHitTestVisibleProperty, false);
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            var text = new FormattedText
+            (
+                Text,
+                CultureInfo.InvariantCulture, FlowDirection.LeftToRight,
+                new Typeface(new FontFamily("Arial"), FontStyles.Normal, FontWeights.Bold, FontStretches.Normal),
+                15, new SolidColorBrush(Colors.Black), VisualTreeHelper.GetDpi(this).PixelsPerDip
+            );
+            var x = AdornedElement.RenderSize.Width / 2 - text.Width / 2;
+            var y = AdornedElement.RenderSize.Height / 2 - text.Height / 2;
+            drawingContext.PushClip(new RectangleGeometry(new Rect(AdornedElement.RenderSize)));
+            drawingContext.DrawText(text, new Point(x, y));
+        }
+    }
+}
